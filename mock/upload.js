f6c14f331380ced62b5a55008b37c5210cf235a6
Sparetire <s9ar371r3@gmail.com>
2017-05-05 19:41:26 +0800

    fix: 细节修改

--- mock/upload.js
+++ mock/upload.js
@@ -1,27 +1,27 @@
-const multer = require('koa-multer');
-const path = require('path');
+// const multer = require('koa-multer');
+// const path = require('path');
 
 
-let storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, path.resolve(__dirname, '../../uploads'));
-	},
-	filename: function (req, file, cb) {
-		cb(null, file.originalname);
-	}
-});
+// let storage = multer.diskStorage({
+// 	destination: function (req, file, cb) {
+// 		cb(null, path.resolve(__dirname, '../../uploads'));
+// 	},
+// 	filename: function (req, file, cb) {
+// 		cb(null, file.originalname);
+// 	}
+// });
 
-function upload(opts) {
-	return async (ctx, next) => {
-		console.log(ctx.req.files);
-		ctx.body = '<script>window.top.sayHello("Hello")</script>';
-		await next();
-	};
-}
+// function upload(opts) {
+// 	return async (ctx, next) => {
+// 		console.log(ctx.req.files);
+// 		ctx.body = '<script>window.top.sayHello("Hello")</script>';
+// 		await next();
+// 	};
+// }
 
-module.exports = {
-	'post /upload': [multer({
-		storage,
-		limits: '5MB'
-	}).single('images'), upload()]
-};
+// module.exports = {
+// 	'post /upload': [multer({
+// 		storage,
+// 		limits: '5MB'
+// 	}).single('images'), upload()]
+// };
